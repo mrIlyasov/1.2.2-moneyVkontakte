96e322d451f12e1b79675fabaa1936f0086592c3
Артур Ильясов <mr.ilyasov90@yandex.ru>
2022-07-11 15:05:35 +0500

    count comission added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,9 @@
-class main {
-}
+
+    fun main(){
+        val min = 35_00
+        val comission = 0.075
+        var amount = 100_00
+        var currentComisssion = amount * comission
+        val result = if (currentComisssion< min) amount - min else amount- currentComisssion
+    println(result)
+    }
