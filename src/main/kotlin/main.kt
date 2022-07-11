5c091fc53c326eb2c1118021d7cc8ebd36ab9b1d
Артур Ильясов <mr.ilyasov90@yandex.ru>
2022-07-11 15:16:46 +0500

    added result in main currency

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
-
-    fun main(){
-        val min = 35_00
-        val comission = 0.075
-        var amount = 100_00
-        var currentComisssion = amount * comission
-        val result = if (currentComisssion< min) amount - min else amount- currentComisssion
-    println(result)
-    }
+fun main() {
+    val min = 35_00
+    val comission = 0.0075
+    var amount = 10000_00
+    var currentComisssion = amount * comission
+    val result: Int = if (currentComisssion < min) (amount - min).toInt() else (amount - currentComisssion).toInt()
+    val resultInCurrency = result/100
+    println(resultInCurrency)
+}
